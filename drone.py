2ead677d88298a9e7690748e933885f20f729cda
Ali Efe <aliefekaragul@gmail.com>
2021-03-18 23:07:32 +0300

    added return feature

--- drone.py
+++ drone.py
@@ -1,34 +1,91 @@
-import pygame
+import keyboard
 import threading
 
 class Drone:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.pos = {"x": x, "y": y}
 
-        self.speed = 0.1
+        self.speed = 0.05
 
         self.keep = True
+        self.returning = False
+
+        # a Vector for storing all movements by axises
+        self.movements = {"x": 0, "y": 0} 
     
+    """
+        Starts drone
+    """
     def Start(self):
         self.keep = True
 
+        # Creating and running a thread for X-Axis movements
         axisx_thread = threading.Thread(target=self.AxisX)
         axisx_thread.start()
 
+        # Creating and running a thread for Y-Axis movements 
+        axisy_thread = threading.Thread(target=self.AxisY)
+        axisy_thread.start()
+        
+        # Creating and running a thread for returning sequence 
+        return_thread = threading.Thread(target=self.Return)
+        return_thread.start()
 
+    """
+        Stops drone
+    """
     def Stop(self):
         self.keep = False
 
-
+    """
+        Get keyboard inputs and record movements in X-Axis
+    """
     def AxisX(self):
         while self.keep:
-            keys = pygame.key.get_pressed()
-            #self.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * self.speed
-            if keys[pygame.K_RIGHT]:
-                self.x += self.speed
+            if not self.returning: # If returning sequence is not active
+                if keyboard.is_pressed('d'):
+                    self.pos["x"] += self.speed
+                    self.movements["x"] += self.speed
 
-            if keys[pygame.K_RIGHT]:
-                self.x -= self.speed
-    
+                if keyboard.is_pressed('a'):
+                    self.pos["x"] -= self.speed
+                    self.movements["x"] -= self.speed
+
+    """
+        Get keyboard inputs and record movements in Y-Axis
+    """
+    def AxisY(self):
+        while self.keep:
+            if not self.returning: # If returning sequence is not active
+                if keyboard.is_pressed('s'):
+                    self.pos["y"] += self.speed
+                    self.movements["y"] += self.speed
+
+                if keyboard.is_pressed('w'):
+                    self.pos["y"] -= self.speed
+                    self.movements["y"] -= self.speed
+
+
+    def Return(self):
+        while self.keep:
+            # Activating and deactivating returning sequence
+            if keyboard.is_pressed('r'):
+                self.returning = True
+            if keyboard.is_pressed('t'):
+                self.returning = False
+
+            # Returning sequence
+            if self.returning:
+                # Stop returning if movements vector is zero
+                if -1 < self.movements["x"] < 1 and -1 < self.movements["y"] < 1:
+                    self.returning = False
+                else:
+                    x_speed = (0 - (self.movements["x"] > 0)) * self.speed
+                    y_speed = (0 - (self.movements["y"] > 0)) * self.speed
+
+                    self.pos["x"] += x_speed
+                    self.movements["x"] += x_speed
+
+                    self.pos["y"] += y_speed
+                    self.movements["y"] += y_speed
 
--- returning_drone.py
+++ returning_drone.py
@@ -3,19 +3,25 @@ from drone import Drone
 
 pygame.init()
 nScreenH = 500
-nScreenW = 500
+nScreenW = 500 
 screen = pygame.display.set_mode([nScreenW, nScreenH])
 
 drone = Drone(nScreenW / 2, nScreenH / 2)
 drone.Start()
 
+i = 0
 running = True
 while running:    
     pygame.display.update()
     screen.fill((255, 255, 255))
 
+    i += 1
+    if i % 1000 == 0:
+        print(drone.movements)
+        i = 0
+
     # Draw drone
-    pygame.draw.circle(screen, (0, 0, 255), (drone.x, drone.y), 10)
+    pygame.draw.circle(screen, (0, 0, 255), (drone.pos["x"], drone.pos["y"]), 10)
 
 
     for event in pygame.event.get():
